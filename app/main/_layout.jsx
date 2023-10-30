3396b6097ae2a43f2e2ac01f24fefb493ed942bb
pedroduranc1 <61203515+pedroduranc1@users.noreply.github.com>
2023-10-30 17:40:30 -0400

    UPDATE 30/10/2023

--- app/main/_layout.jsx
+++ app/main/_layout.jsx
@@ -7,6 +7,7 @@ import { Image } from "expo-image";
 export default () => {
   return (
     <Tabs
+      initialRouteName="home"
       screenOptions={{
         headerShown: false,
         tabBarShowLabel: false,
--- app/main/home.jsx
+++ app/main/home.jsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import TobBarOptions from "../../src/components/ui/TobBarOptions";
+import ContainerFondo from "../../src/components/ui/ContainerFondo";
 import { Image } from "expo-image";
-import buho from "../../assets/logonuevo.webp";
-import { useQuery, useQueryClient } from "react-query";
+import logo from "../../assets/logocolor.webp";
+import { router } from "expo-router";
 
 const home = () => {
   // const queryCl = useQueryClient();
@@ -12,12 +13,41 @@ const home = () => {
   // const { data: User } = useQuery(token, async () => UserCtrl.getMe(token));
 
   return (
-    <>
-      <TobBarOptions route={"home"} />
-      <View className="flex flex-1 w-[90%] mx-auto">
-        
+    <ContainerFondo>
+      <View className="flex w-full flex-1 items-center ">
+        <Image source={logo} className="w-[260px] h-[150px] my-[30%]" />
+
+        <View className="w-full flex items-center gap-y-3 flex-1 px-[5%] ">
+          <TouchableOpacity
+          onPress={()=> router.replace("/main/informes")}
+          className="border-2 border-LogoBlue bg-white hover:bg-LogoBlue group rounded-full w-full py-2 flex justify-center items-center"
+          >
+            <Text className="text-LogoBlue font-bold group-hover:text-white">Ingresos y Gastos</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+          className="border-2 border-LogoBlue bg-white  rounded-full w-full py-2 flex justify-center items-center"
+          >
+            <Text className="text-LogoBlue font-bold">Declaracion de Impuestos</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+          className="border-2 border-LogoBlue bg-white  rounded-full w-full py-2 flex justify-center items-center"
+          >
+            <Text className="text-LogoBlue font-bold">Informes</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+          onPress={()=> router.replace("/main/documentos")}
+          className="border-2 border-LogoBlue bg-white  rounded-full w-full py-2 flex justify-center items-center"
+          >
+            <Text className="text-LogoBlue font-bold">Documentos</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+          className="border-2 border-LogoBlue bg-white  rounded-full w-full py-2 flex justify-center items-center"
+          >
+            <Text className="text-LogoBlue font-bold">Otros</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </>
+    </ContainerFondo>
   );
 };
 
